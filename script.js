ddaafb410bd8e10727a4635a53847e8d64b4db6a
aaronh1986 <aaronharbourne@gmail.com>
2021-04-29 12:20:40 +0000

    Finish html

--- script.js
+++
@@ -1,4 +0,0 @@
-let button = document.getElementsByTagName("button")[0].addEventListener('click', function() {
-    let dropdown = document.getElementById('dropdown').innerHTML = `<h2>Your second heading here here here here here</h2>`;
-    dropdown
-});
